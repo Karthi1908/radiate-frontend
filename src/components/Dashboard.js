bac313614b4a252ec2ad8dcac057aea62189f238
nishantchandla <nishantchandla6@gmail.com>
2021-08-27 12:48:44 +0530

    Fixes: Bug, dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,10 +3,10 @@ import '../css/dashboard.css';
 
 const Dashboard = () => {
     return (
-        <div className="container main-section">
+        <div className="container container-content main-section">
             <div className="col" align="center">
                 <div className="h5 text">Sign in with your tezos account to view incoming streams</div>
-                <button type="button" onClick={{/* popup */}} class="btn btn-primary">Sign in</button>
+                <button type="button" onClick={()=>{}} className="btn btn-primary">Sign in</button>
             </div>
         </div>
     );

b5f2b133804477e4976992171dbc84ceca33aaef
nishantchandla <nishantchandla6@gmail.com>
2021-08-29 16:58:35 +0530

    Added: History modal

--- src/components/StreamDetails.js
+++ src/components/StreamDetails.js
@@ -3,15 +3,19 @@ import { useParams } from 'react-router';
 import { createClient, everything } from 'radiate-finance-sdk';
 import { withdraw, cancelStream } from '../actions';
 import { useSelector, useDispatch } from 'react-redux';
+import '../css/stream-details.css';
 
 const StreamDetails = () => {
     let { streamID } = useParams();
     const [stream, setStream] = useState(null);
     const [flow, setFlow] = useState(0);
     const dispatch = useDispatch();
+    const refModel = useRef();
     
 
     useEffect(() => {
+       
+        // refModel.current;
         const create = createClient({
             url: 'wss://hasura-radiateapi.herokuapp.com/v1/graphql'
         });
@@ -21,11 +25,13 @@ const StreamDetails = () => {
 
         }).get({...everything}).subscribe(e => {
             setStream(e[0]);
-            setFlow(e[0].remainingBalance/1000000);
             setInterval(()=>{
                 let timeNow= (new Date()).getTime()/1000;
-                let amount_now=parseFloat(((((timeNow-(Date.parse(e[0].startTime))/1000)*e[0].ratePerSecond)-(e[0].deposit-e[0].remainingBalance))/1000000)).toFixed(6);   
-                setFlow(`${amount_now} Tez`);},20);
+                let amount_now=parseFloat(((((timeNow-(Date.parse(e[0].startTime))/1000)*e[0].ratePerSecond)-(e[0].deposit-e[0].remainingBalance))/1000000)).toFixed(6); 
+                if(parseFloat(amount_now)>0){
+                    setFlow(`${amount_now} Tez`);
+                }
+                },20);
             // console.log(e[0]);
         }))();
     }, []);
@@ -34,9 +40,9 @@ const StreamDetails = () => {
         return (
             <div className="container container-content main-section">
                 Loading..
-                <div class="spinner-border" role="status">
+                <div className="spinner-border" role="status">
                     
-                    <span class="visually-hidden">Loading...</span>
+                    <span className="visually-hidden">Loading...</span>
                 </div>
             </div>
         );
@@ -49,44 +55,61 @@ const StreamDetails = () => {
                 <div className="container main-section" style={{height:'80vh'}}><span>No such stream exists</span></div>
             ):(
                 <div className="container">
-                     <div class="row">
-
-                        <div class="col-10">
-                            <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{flow}</h5>
-                                        <p class="card-text"></p>
+            
+                     <div className="row align-items-center" style={{height:'80vh'}}>
+                        <div className="col-10">
+                            <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{flow}</h5>
+                                        <p className="card-text"></p>
                                     </div>
                             </div>
                         </div>
 
-                        <div class="col">
-                            <div class="col">
-                                <div class="card" onClick={()=>dispatch(withdraw())}>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Withdraw</h5>
-                                        <p class="card-text"></p>
+                        <div className="col">
+                            <div className="col">
+                                <div className="card btn" onClick={()=>dispatch(withdraw())}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Withdraw</h5>
+                                        <p className="card-text"></p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">History</h5>
-                                        <p class="card-text"></p>
+                            <div className="col">
+                                <div className="card btn"  data-bs-toggle="modal" data-bs-target="#historyModal">
+                                    <div className="card-body">
+                                        <h5 className="card-title">History</h5>
+                                        <p className="card-text"></p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col" >
-                                <div class="card btn" onClick={()=>{dispatch(cancelStream({streamId: stream.streamId}));}}>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Cancel Stream</h5>
-                                        <p class="card-text"></p>
+                            <div className="col" >
+                                <div className="card btn" onClick={()=>{dispatch(cancelStream({streamId: stream.streamId}));}}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Cancel Stream</h5>
+                                        <p className="card-text"></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+            <div className="modal" id="historyModal" tabIndex="-1"  aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" >
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Modal title</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <p>Modal body text goes here.</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+            </div>
+                </div>
                 </div>
             )}
         </div>
